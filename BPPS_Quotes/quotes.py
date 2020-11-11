61da4d2dfe6a80a2516e2770df60bb8231728db7
lvgvt <lvg1159@gmail.com>
2020-11-11 17:20:33 -0500

    added quote cog

--- BPPS_Quotes/quotes.py
+++ BPPS_Quotes/quotes.py
@@ -1,10 +1,46 @@
 from redbot.core import commands
+import sqlite3
+from sqlite3 import Error
 
 class quotes(commands.Cog):
     """My custom cog"""
+    database = r"../bppssqlite.db"
+
+    # create a database connection
+    conn = create_connection(database)
 
     @commands.command()
     async def quote(self, ctx):
         """This does stuff!"""
+        quote = select_random_quote(conn)
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send(quote)
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def select_random_quote(conn):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT quote FROM table ORDER BY RANDOM() LIMIT 1")
+
+    rows = cur.fetchall()
+
+    return rows

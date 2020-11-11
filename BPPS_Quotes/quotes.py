e790df851a956384e35aa6d3ab7ea48755d3edd5
lvgvt <lvg1159@gmail.com>
2020-11-11 17:46:35 -0500

    reordered input

--- BPPS_Quotes/quotes.py
+++ BPPS_Quotes/quotes.py
@@ -2,21 +2,6 @@ from redbot.core import commands
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return conn
-
-
 def select_random_quote(conn):
     """
     Query tasks by priority
@@ -24,6 +9,13 @@ def select_random_quote(conn):
     :param priority:
     :return:
     """
+
+    conn = None
+    try:
+        conn = sqlite3.connect(r"./bppssqlite.db")
+    except Error as e:
+        print(e)
+
     cur = conn.cursor()
     cur.execute("SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1")
 
@@ -37,12 +29,6 @@ class quotes(commands.Cog):
     @commands.command()
     async def quote(self, ctx):
         """This does stuff!"""
-
-        database = r"./bppssqlite.db"
-
-        # create a database connection
-        conn = create_connection(database)
-
         quote = select_random_quote(conn)
         # Your code will go here
         await ctx.send(quote)

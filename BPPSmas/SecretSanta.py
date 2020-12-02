7ac83bce80209a5285a83b4fcd7067f9aca15310
lvgvt <lvg1159@gmail.com>
2020-12-02 17:48:00 +0000

    cahnges

--- BPPSmas/SecretSanta.py
+++ BPPSmas/SecretSanta.py
@@ -1,5 +1,4 @@
 from redbot.core import commands
-import random
 
 def addEntry(name):
     names = open('secretsanta.txt', 'a')
@@ -7,16 +6,20 @@ def addEntry(name):
     names.close()
     return "Chess patch note added to the chain!"
 
-def sendDm():
+def sendDm(bot):
     names = open('secretsanta.txt', 'r')
-    user = names.readline()
-    user.send("hello, "+user)
+    userID = names.readline()
+    userID = userID[2:len(userID)-2]
+    bot.get_user(userID).send("hello")
     names.close()
-    return "DM'ed"
+    return userID
 
 
 class SecretSanta(commands.Cog):
     """Chess Patch Notes!"""
+    def __init__(self, bot):
+        self.bot = bot
+
 
     @commands.command()
     async def addSanta(self, ctx, user):
@@ -26,4 +29,4 @@ class SecretSanta(commands.Cog):
 
     @commands.command()
     async def santaTest(self, ctx):
-        sendDm()
+        await ctx.send("DM'ed ID: "+sendDm(self.bot))

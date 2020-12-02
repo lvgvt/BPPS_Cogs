12343c1ebdf5d486d9384a02b48bccc70becbde8
lvgvt <lvg1159@gmail.com>
2020-12-02 14:09:24 -0500

    udpated

--- BPPSmas/SecretSanta.py
+++ BPPSmas/SecretSanta.py
@@ -25,6 +25,14 @@ class SecretSanta(commands.Cog):
         for i in range(len(allUsers)-1):
             userToSend = allUsers[i]
             userToRecieve = allUsers[i+1]
-            await user.send("Hello <@"+str(userToSend.id)+">, you are the secret santa for "+userToRecieve.name+" to send them a gift!")
+            await user.send("Hello <@"+str(userToSend.id)+">, you are the secret santa for **"+userToRecieve.name+"** to send them a gift!")
             await user.sent("Consider dropping off their present at their house while social distancing, or send my mail.")
             await user.send("Merry Christmas, Happy Holidays!")
+
+    @commands.command()
+    async def addSanta(self, ctx, user):
+        # Your code will go here
+        names = open('secretsanta.txt', 'a')
+        names.write(user+'\n')
+        names.close()
+        await ctx.send(user+' has been added')

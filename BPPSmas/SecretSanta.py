9faaa547eed25a289eae9fc5af08bac1ea5b33f4
lvgvt <lvg1159@gmail.com>
2020-12-02 13:50:41 -0500

    finished logic

--- BPPSmas/SecretSanta.py
+++ BPPSmas/SecretSanta.py
@@ -1,6 +1,6 @@
 import discord
-import datetime
 from redbot.core import commands
+import random
 
 __spiced_up_by__ = "Young™#5484"
 __author__ = "Neo#1375"
@@ -11,11 +11,18 @@ class SecretSanta(commands.Cog):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def santaTest(self, ctx):
+    async def sendSantas(self, ctx):
         """Dm users."""
-        names = open('secretsanta.txt', 'r')
-        user_id = names.readline()
-        user_id = user_id[3:len(user_id)-2]
-        print(self.bot.users)
-        user = self.bot.get_user(int(user_id))
-        await user.send("hello!")
+        allUsers = []
+        f = open('secretsanta.txt', 'r')
+        for line in f:
+            user_id = int(line[3:len(line)-2])
+            user = self.bot.get_user(user_id)
+            allUsers.append(user)
+        random.shuffle(allUsers)
+        allUsers.append(allUsers[0])
+
+        for i in range(len(allUsers-1)):
+            userToSend = allUsers[i]
+            userToRecieve = allUsers[i+1]
+            await user.send("Hello "+userToSend.name+", you are the secret santa for "+userToRecieve+" to send them a gift! Consider dropping off their present while social distancing at their house, or send my mail. Merry Christmas, Happy Holidays!")
--- BPPSmas/__init__.py
+++ BPPSmas/__init__.py
@@ -1,6 +1,5 @@
 from .SecretSanta import SecretSanta
 
 def setup(bot):
-    bot.remove_command('whisper')
     n = SecretSanta(bot)
     bot.add_cog(n)

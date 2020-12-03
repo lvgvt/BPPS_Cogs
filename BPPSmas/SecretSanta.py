245aba47562cb48d2dea04afb1499f4713fe813c
lvgvt <lvg1159@gmail.com>
2020-12-02 19:58:40 -0500

    formatting

--- BPPSmas/SecretSanta.py
+++ BPPSmas/SecretSanta.py
@@ -10,6 +10,7 @@ class SecretSanta(commands.Cog):
     @commands.command(pass_context=True)
     async def sendSantas(self, ctx):
         """Dm users."""
+        #determine secret santa order
         allUsers = []
         f = open('secretsanta.txt', 'r')
         for line in f:
@@ -18,13 +19,23 @@ class SecretSanta(commands.Cog):
             allUsers.append(user)
         random.shuffle(allUsers)
         allUsers.append(allUsers[0])
+        f.close()
 
+        #record results just in case
+        f = open('secretsanta.txt', 'a')
+        for user in allUsers:
+            f.append(str(user.name)+" | ")
+        f.close()
+
+        #dm users
         for i in range(len(allUsers)-1):
             userToSend = allUsers[i]
             userToRecieve = allUsers[i+1]
             await user.send("Hello <@"+str(userToSend.id)+">, you are the secret santa for **"+userToRecieve.name+"** to send them a gift!")
-            await user.send("Consider dropping off their present at their house while social distancing, or send my mail.")
-            await user.send("Merry Christmas, Happy Holidays!")
+            await user.send("Consider sending by mail, or if you live closeby you can drop it off")
+            await user.send("If you need an address or need to inform someone a package has been delivered, feel free to ask Inabiaf or PointAndGlick to ask on your behalf so you can maintain anonymity.")
+            await user.send("When you recieve your gift, *DON'T OPEN IT!* We will be holding a discord call w/ facecam sometime after Christmas where we will open all ours up.")
+            await user.send("Merry Christmas, Happy Holidays! :christmas_tree:")
 
     @commands.command()
     async def addSanta(self, ctx, user):
